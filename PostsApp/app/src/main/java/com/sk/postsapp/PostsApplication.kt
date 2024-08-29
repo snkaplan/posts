2747404f464c639dffbb8961f217b29adb84c1e0
skaplan <sinan.kaplan@loodos.com>
2024-08-29 15:43:07 +0300

    disable dark theme

--- PostsApp/app/src/main/java/com/sk/postsapp/PostsApplication.kt
+++ PostsApp/app/src/main/java/com/sk/postsapp/PostsApplication.kt
@@ -1,7 +1,13 @@
 package com.sk.postsapp
 
 import android.app.Application
+import androidx.appcompat.app.AppCompatDelegate
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class PostsApplication : Application()
+class PostsApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+    }
+}
